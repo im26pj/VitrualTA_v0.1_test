d629215a7d0d00893cb75f0eacb5a9a0ccd4120a
喵嘿 <411135050@gms.ndhu.edu.tw>
2025-05-14 17:24:12 +0800

    圖片api

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 var mongoose = require('mongoose');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
-
 var app = express();
 
 // ======== 加入靜態前端檔案支援 ========
@@ -47,15 +45,16 @@ app.use(cookieParser());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/pict', express.static(path.join(__dirname, 'public/pict')));
 
 // 掛載 api 路由
 app.use('/api', apiRouter);
 app.use('/users', usersRouter);
+app.use('/api/image', imageRoutes);
 app.use('/', indexRouter);
 
 // ======== 所有非 API 路由交給 React 處理 ========
